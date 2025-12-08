ecbd640b7b54e8780e6e994c81b2ba87bb0e20d1
AmirGillani <amirgillani160@gmail.com>
2025-12-08 17:27:24 +0500

    build error resolved

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -50,24 +50,6 @@ type ApiRouteConfig = {
   type __Unused = __Check
 }
 
-// Validate ../../pages/_app.tsx
-{
-  type __IsExpected<Specific extends PagesPageConfig> = Specific
-  const handler = {} as typeof import("../../pages/_app.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
-// Validate ../../pages/_document.tsx
-{
-  type __IsExpected<Specific extends PagesPageConfig> = Specific
-  const handler = {} as typeof import("../../pages/_document.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../pages/about.tsx
 {
   type __IsExpected<Specific extends PagesPageConfig> = Specific
