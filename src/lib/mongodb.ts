c2b16c4a286afc62229198ed3f0819bc813b64eb
AmirGillani <amirgillani160@gmail.com>
2025-12-12 18:20:16 +0500

    clean up

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -17,10 +17,10 @@ interface MongooseCache {
 }
 
 declare global {
-  var mongoose: MongooseCache;
+  var mongoose: MongooseCache | undefined;
 }
 
-let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 
 if (!global.mongoose) {
   global.mongoose = cached;
